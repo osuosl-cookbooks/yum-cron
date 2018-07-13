2c289aa98288b933564402084635930dfc2ab656
jerrypeng0112 <jerrypeng0112@hotmail.com>
2018-07-13 13:30:52 -0700

    Switch to cookstyle and apply fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,53 +22,53 @@ when 7
     'update_messages' => 'yes',
     'download_updates' => 'yes',
     'apply_updates' => 'yes',
-    'random_sleep' => 360
+    'random_sleep' => 360,
   }
   default['yum-cron']['daily']['emitters'] = {
     'system_name' => 'None',
     'emit_via' => 'stdio',
-    'output_width' => 80
+    'output_width' => 80,
   }
   default['yum-cron']['daily']['email'] = {
     'email_from' => 'root@localhost',
     'email_to' => 'root',
-    'email_host' => 'localhost'
+    'email_host' => 'localhost',
   }
   default['yum-cron']['daily']['groups'] = {
     'group_list' => 'None',
-    'group_package_types' => 'mandatory, default'
+    'group_package_types' => 'mandatory, default',
   }
   default['yum-cron']['daily']['base'] = {
     'debuglevel' => '-2',
     'skip_broken' => 'False',
     'mdpolicy' => 'group:main',
-    'assumeyes' => 'False'
+    'assumeyes' => 'False',
   }
   default['yum-cron']['hourly']['commands'] = {
     'update_cmd' => 'default',
     'update_messages' => 'no',
     'download_updates' => 'no',
     'apply_updates' => 'no',
-    'random_sleep' => 15
+    'random_sleep' => 15,
   }
   default['yum-cron']['hourly']['emitters'] = {
     'system_name' => 'None',
     'emit_via' => 'stdio',
-    'output_width' => 80
+    'output_width' => 80,
   }
   default['yum-cron']['hourly']['email'] = {
     'email_from' => 'root@localhost',
     'email_to' => 'root',
-    'email_host' => 'localhost'
+    'email_host' => 'localhost',
   }
   default['yum-cron']['hourly']['groups'] = {
     'group_list' => 'None',
-    'group_package_types' => 'mandatory, default'
+    'group_package_types' => 'mandatory, default',
   }
   default['yum-cron']['hourly']['base'] = {
     'debuglevel' => '-2',
     'skip_broken' => 'False',
     'mdpolicy' => 'group:main',
-    'assumeyes' => 'False'
+    'assumeyes' => 'False',
   }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,11 @@ ChefSpec::Coverage.start! { add_filter 'yum-cron' }
 CENTOS_7_OPTS = {
   platform: 'centos',
   version: '7.2.1511',
-  log_level: :fatal
+  log_level: :fatal,
 }.freeze
 
 CENTOS_6_OPTS = {
   platform: 'centos',
   version: '6.7',
-  log_level: :fatal
+  log_level: :fatal,
 }.freeze
