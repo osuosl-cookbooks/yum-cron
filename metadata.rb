17c41d49a1927439d71e3b6ecc76500f4d755ba1
Ryan <kennedy@osuosl.org>
2021-04-06 09:14:23 -0700

    Update Chef dependency to >= 16

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@osuosl.org'
 issues_url       'https://github.com/osuosl-cookbooks/yum-cron/issues'
 source_url       'https://github.com/osuosl-cookbooks/yum-cron'
 license          'Apache-2.0'
-chef_version     '>= 14.0'
+chef_version     '>= 16.0'
 description      'Installs/Configures yum-cron'
 version          '2.0.0'
 
